c5da8e2f6bf5c53742f294954cc30392c176788e
Felix Carrasco <felix.marcial@gmail.com>
2019-05-31 16:01:13 -0300

    Paso 8
    Se agregan graficos diarios de temperatura máxima y mínima desde el dia realizado hasta siete más.
    Se agrega viento en cada hora, coloreando desde 10m/s en adelante.

--- extra_sinop_funciones.py
+++ extra_sinop_funciones.py
@@ -46,7 +46,7 @@ def extraer_variable(file, fecha, nomvar, llat, llon):
             i_t1 = [i for i in range(len(tiempos)) if tiempos[i] == di][0]
             i_t2 = [i for i in range(len(tiempos)) if tiempos[i] == di_f][0]
             fdates.append(di)
-            res[dia,:,:] = np.ma.sum(ppinit[i_t1:i_t2,:,:], axis=0)
+            res[dia, :, :] = np.ma.sum(ppinit[i_t1:i_t2, :, :], axis=0)
     elif nomvar == 'tmax':
         # Leemos la variable
         tmax2m = file.variables['tmax2m'][:,i_lat[0]:i_lat[1]+1,
@@ -58,7 +58,7 @@ def extraer_variable(file, fecha, nomvar, llat, llon):
             i_t1 = [i for i in range(len(tiempos)) if tiempos[i] == di][0]
             i_t2 = [i for i in range(len(tiempos)) if tiempos[i] == di_f][0]
             fdates.append(di)
-            res[dia,:,:] = np.ma.max(tmax2m[i_t1:i_t2,:,:], axis=0)
+            res[dia, :, :] = np.ma.max(tmax2m[i_t1:i_t2, :, :], axis=0)
     elif nomvar == 'tmin':
         tmin2m = file.variables['tmin2m'][:,i_lat[0]:i_lat[1]+1,
                                           i_lon[0]:i_lon[1]+1]
@@ -66,17 +66,40 @@ def extraer_variable(file, fecha, nomvar, llat, llon):
         for dia in np.arange(0, 7):
             di = d0 + dt.timedelta(days=int(dia))
             di_f = (di + dt.timedelta(days=1)).replace(hour=0)
-            print(di)
-            print(di_f)
             i_t1 = [i for i in range(len(tiempos)) if tiempos[i] == di][0]
             i_t2 = [i for i in range(len(tiempos)) if tiempos[i] == di_f][0]
-            res[dia,:,:] = np.ma.min(tmin2m[i_t1:i_t2,:,:], axis=0)
+            fdates.append(di)
+            res[dia, :, :] = np.ma.min(tmin2m[i_t1:i_t2, :, :], axis=0)
     else:
         print('Solo hay programado para Precip, Tmax y Tmin diaria')
         print('Se devuelve una matriz con NaN')
     ###### End of OPTIONs ##############
 
     return res, fdates
+
+
+def extract_wind(file, fecha, llat, llon):
+    """
+    """
+    l_lat = llat
+    l_lon = np.array(llon) % 360
+    i_lat, i_lon, lat, lon = get_index_lat(fecha, file, llat, llon)
+    tiempos = get_index_time(file, fecha)
+    di = tiempos[1]
+    di_f = (di + dt.timedelta(days=7)).replace(hour=0)
+    i_t1 = [i for i in range(len(tiempos)) if tiempos[i] == di][0]
+    i_t2 = [i for i in range(len(tiempos)) if tiempos[i] == di_f][0]
+    # Creamos una variable aux
+    res = np.empty([i_t2 - i_t1 + 1, len(lat), len(lon)])
+    res[:] = np.nan
+    fdates = [datef for datef in tiempos if datef>=di and datef<=di_f]
+    u10m = file.variables['ugrd10m'][i_t1:i_t2, i_lat[0]:i_lat[1]+1,
+                                     i_lon[0]:i_lon[1]+1]
+    v10m = file.variables['vgrd10m'][i_t1:i_t2, i_lat[0]:i_lat[1]+1,
+                                     i_lon[0]:i_lon[1]+1]
+
+    return lon, lat, u10m, v10m, fdates
+
 
 
 def plot_precip_daily(file, llat, llon, fecha, carpeta):
@@ -113,10 +136,95 @@ def plot_precip_daily(file, llat, llon, fecha, carpeta):
         plt.savefig(fig_name, dpi=200)
 
 
+def plot_temp_daily(file, llat, llon, fecha, carpeta):
+    """
+    Estimates daily precipitation for each day since start till seven days later
+    and plot it.
+    """
+    # Obtenemos las variables a graficar
+    TMAX, fdates = extraer_variable(file, fecha, 'tmax', llat, llon)
+    TMIN, fdates = extraer_variable(file, fecha, 'tmin', llat, llon)
+    # Shape:(7,Lat,Lon)
+    lndsfc = extract_var(fecha, file, llat, llon, 'lndsfc')
+    i_lat, i_lon, lat, lon = get_index_lat(fecha, file, llat, llon)
+    # Datos para el Mapa
+    fig1, ax1 = mapa_base(llat, llon)
+    fig2, ax2 = mapa_base(llat, llon)
+    # de 0-360 a -180 - 180 en Longitud
+    x = ((np.squeeze(np.asarray(lon)) - 180) % 360) - 180
+    y = np.squeeze(np.asarray(lat))
+    bounds = np.arange(-18, 46, 2)
+    cMap = c.ListedColormap(['#3A027E', '#480092', '#06187B', '#162892',
+                             '#203CB7', '#2A50C1', '#3066DB', '#417AF8',
+                             '#508CF9', '#619FF8', '#76B2F2', '#87C6FD',
+                             '#93DEFE', '#ACECF7', '#C7F4F8', '#93E4B0',
+                             '#A5F789', '#D1FC7A', '#E7EE90', '#FCE5A1',
+                             '#FBCB61', '#FBB06D', '#FF7B40', '#FC5C2A',
+                             '#FF3A1D', '#DE0406', '#B80004', '#960103',
+                             '#6B0207', '#450102', '#250400'])
+    norm = c.BoundaryNorm(boundaries=bounds, ncolors=31)
+    for t in np.arange(0, np.shape(TMAX)[0]):
+        z1 = TMAX[t, :, :] - 273.
+        z1[lndsfc == 0.] = np.nan
+        #print(np.nanmin(z1), np.nanmax(z1))
+        z2 = TMIN[t, :, :] - 273.
+        z2[lndsfc == 0.] = np.nan
+        #print(np.nanmin(z2), np.nanmax(z2))
+        # Tmax
+        CS1 = ax1.contourf(x, y, z1, levels=bounds, cmap=cMap, norm=norm,
+                           transform=ccrs.PlateCarree())
+        cbaxes = inset_axes(ax1, width="3%", height="50%", loc=4)
+        cb = fig1.colorbar(CS1, cax=cbaxes, drawedges=True, ticks=bounds)
+        cb.ax.yaxis.set_ticks_position('left')
+        for label in cb.ax.yaxis.get_ticklabels()[::2]:
+            label.set_visible(False)
+        fig_name = carpeta + 'TMAX_' + fdates[t].strftime('%Y%m%d') + '.png'
+        fig1.savefig(fig_name, dpi=200)
+        # Tmin
+        CS2 = ax2.contourf(x, y, z2, levels=bounds, cmap=cMap, norm=norm,
+                           transform=ccrs.PlateCarree())
+        cbaxes1 = inset_axes(ax2, width="3%", height="50%", loc=4)
+        cb1 = fig1.colorbar(CS2, cax=cbaxes1, drawedges=True, ticks=bounds)
+        cb1.ax.yaxis.set_ticks_position('left')
+        for label in cb1.ax.yaxis.get_ticklabels()[::2]:
+            label.set_visible(False)
+        fig_name = carpeta + 'TMIN_' + fdates[t].strftime('%Y%m%d') + '.png'
+        fig2.savefig(fig_name, dpi=200)
+
+
+def plot_wind_daily(file, llat, llon, fecha, carpeta):
+    """
+    """
+    lon, lat, u, v, fdates = extract_wind(file, fecha, llat, llon)
+    # Datos Mapa
+    fig, ax = mapa_base(llat, llon)
+    x = ((np.squeeze(np.asarray(lon)) - 180) % 360) - 180
+    y = np.squeeze(np.asarray(lat))
+    wspd = np.sqrt(np.power(u, 2) + np.power(v, 2))
+    cMap = c.ListedColormap(['#ffffff', '#b3cde3', '#8c96c6', '#8856a7',
+                             '#810f7c'])
+    bounds = [0., 10., 13., 16., 20., 50.]
+    norm = c.BoundaryNorm(boundaries=bounds, ncolors=5)
+    skip=(slice(None,2), slice(None,2))
+    for t in np.arange(0, np.shape(u)[0]):
+        ws10 = wspd[t, :, :]
+        u10 = u[t, :, :]/ws10
+        v10 = v[t, :, :]/ws10
+        CS = ax.contourf(x, y, ws10, levels=bounds, cmap=cMap, norm=norm,
+                         transform=ccrs.PlateCarree())
+        QW = ax.quiver(x[::3], y[::3], u10[::3, ::3], v10[::3, ::3],
+                       transform=ccrs.PlateCarree())
+        cbaxes = inset_axes(ax, width="3%", height="30%", loc=4)
+        cb = fig.colorbar(CS, cax=cbaxes, drawedges=True, ticks=bounds[0:-1])
+        cb.ax.yaxis.set_ticks_position('left')
+        fig_name = carpeta + 'SurfWind_' + fdates[t].strftime('%Y%m%d%H') + '.png'
+        fig.savefig(fig_name, dpi=200)
+
+
 if __name__ == '__main__':
     import os
     #
-    mydate='20190530'
+    mydate='20190531'
     l_lat = [-60., -20.]
     l_lon = [-80., -50.]
     ofolder = 'e:/python/graficos_sinopticos/' + mydate + '/'
@@ -126,4 +234,6 @@ if __name__ == '__main__':
     file = netCDF4.Dataset(url)
     #extract_variable(file, mydate, 'tmax', l_lat, l_lon)
     plot_precip_daily(file, l_lat, l_lon, mydate, ofolder)
+    plot_temp_daily(file, l_lat, l_lon, mydate, ofolder)
+    plot_wind_daily(file, l_lat, l_lon, mydate, ofolder)
     file.close()
